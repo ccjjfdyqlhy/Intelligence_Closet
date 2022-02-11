d28de1ebb436bae1b4f20e4857ab742872ad13a9
DakstarXD <69450668+ccjjfdyqlhy@users.noreply.github.com>
2022-02-12 00:45:58 +0800

    修改一项严重文件读取错误.

--- main.py
+++ main.py
@@ -15,9 +15,10 @@ except ModuleNotFoundError:
 print('[信息]正在读取配置文件...')
 try:
     path=sys.path[4]
-    verify=linecache.getline('\ICconfig.\setup', 1)
-    first=linecache.getline('\ICconfig\.setup', 2)
-    target=linecache.getline('\ICconfig\.setup',3)
+    verify=linecache.getline('\ICconfig\setup', 1)
+    first=linecache.getline('\ICconfig\setup', 2)
+    target=linecache.getline('\ICconfig\setup',3)
+    open ('setup','r')
 except FileNotFoundError:
     print('[错误]没有侦测到配置文件')
     print('[信息]新窗口已创建')
@@ -39,13 +40,13 @@ else:
 print('[信息]正在检查运行次数')
 if first == '1':
     firstrun=1
-    with open(path+'\ICconfig\.setup')as fp:
+    with open(path+'\ICconfig\setup')as fp:
         s = fp.read()#将指定文件读入内存
         fp.close()#关闭该文件
     a = s.split('\n')
     a.insert(1, '0')#在第 LINE+1 行插入
     s = '\n'.join(a)#用'\n'连接各个元素
-    with open(path+'\ICconfig\.setup', 'w')as fob:
+    with open(path+'\ICconfig\setup', 'w')as fob:
         fp.write(s)
         fob.close()
 else:
