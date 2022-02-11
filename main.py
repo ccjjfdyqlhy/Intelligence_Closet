616251ae192d8b79179b3143fe70cfb09aebaab1
DakstarXD <69450668+ccjjfdyqlhy@users.noreply.github.com>
2022-02-11 21:35:54 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 print('[警告]服务已启动!')
-import tkinter
-import requests
-import time
-import sys
-import os
-from tkinter import ttk
-from tkinter import *
-from tkinter.messagebox import *
+try:
+    import tkinter
+    import requests
+    import time
+    import linecache
+    import sys
+    import os
+    from tkinter import ttk
+    from tkinter import *
+    from tkinter.messagebox import *
+except ModuleNotFoundError:
+    print('[错误]无法启动服务，因为程序文件不完整。重新安装应用支持库来解决。')
+    quit()
 print('[信息]正在读取配置文件...')
 try:
-    with open('setup') as file_obj:
-        target = file_obj.read()  
-except:
-    print('[错误]没有侦测到setup文件')
+    path=sys.path[4]
+    verify=linecache.getline('\ICconfig\setup', 1)
+    first=linecache.getline('\ICconfig\setup', 2)
+    target=linecache.getline('\ICconfig\setup',3)
+except FileNotFoundError:
+    print('[错误]没有侦测到配置文件')
     print('[信息]新窗口已创建')
     root=tkinter.Tk()
     root.title('初始化应用程序')
@@ -23,6 +30,27 @@ except:
     root.mainloop()
     print('[警告]程序已退出')
     quit()
+print('[信息]正在验证程序包...')
+if verify == '[ICsetupfile]':
+    pass
+else:
+    print('[警告]配置文件没有被授权，请检查setup.py来源是否可靠')
+    input('[等待]若要忽略请回车->')
+print('[信息]正在检查运行次数')
+if first == '1':
+    firstrun=1
+    with open(path+'\ICconfig\setup')as fp:
+        s = fp.read()#将指定文件读入内存
+        fp.close()#关闭该文件
+    a = s.split('\n')
+    a.insert(1, '0')#在第 LINE+1 行插入
+    s = '\n'.join(a)#用'\n'连接各个元素
+    with open(path+'\ICconfig\setup', 'w')as fob:
+        fp.write(s)
+        fob.close()
+else:
+    firstrun=0
+print('[信息]运行前检查已完成，正在启动天气模块')
 print('[信息]天气模块已启动，正在获取...')
 rep = requests.get('http://www.tianqiapi.com/api?version=v6&appid=99245552&appsecret=RL8NtmPx&city='+target)
 rep.encoding = 'utf-8'
