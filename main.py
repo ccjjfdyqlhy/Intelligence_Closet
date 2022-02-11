ce8ca31939ab28aba28ef8125c7a848fa9a02581
DakstarXD <69450668+ccjjfdyqlhy@users.noreply.github.com>
2022-02-12 01:44:37 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print('[警告]服务已启动!')
+print('[main][警告]服务已启动!')
 try:
     import tkinter
     import requests
@@ -10,18 +10,22 @@ try:
     from tkinter import *
     from tkinter.messagebox import *
 except ModuleNotFoundError:
-    print('[错误]无法启动服务，因为程序文件不完整。重新安装应用支持库来解决。')
+    print('[main][错误]无法启动服务，因为程序文件不完整。重新安装应用支持库来解决')
     quit()
-print('[信息]正在读取配置文件...')
+print('[App][Core][信息]正在读取配置文件...')
 try:
     path=os.getcwd()
     verify=linecache.getline(path+'\config.ICset', 1)
     first=linecache.getline(path+'\config.ICset', 2)
     target=linecache.getline(path+'\config.ICset',3)
-    open(path+'\config.ICset')
+    ask=input('[App][Code Fixer][等待]是否要跳过侦测配置文件？这可以解决大多数代码错误，但有可能会让你失去你的数据！(Y/其他)')
+    if ask == 'Y':
+        print('[App][Code Fixer][信息]已经跳过侦测配置文件')
+    else:
+        open(path+'\config.ICset')
 except FileNotFoundError:
-    print('[错误]没有侦测到配置文件')
-    print('[信息]新窗口已创建')
+    print('[App][Core][错误]没有侦测到配置文件')
+    print('[App][WMG][信息]新窗口已创建')
     root=tkinter.Tk()
     root.title('初始化应用程序')
     root.geometry('500x500')
@@ -29,15 +33,15 @@ except FileNotFoundError:
     label.pack()
     showerror(title='程序错误',message='程序初始化失败，请尝试重新运行setup.py解决！')
     root.mainloop()
-    print('[警告]程序已退出')
+    print('[main][警告]程序已退出')
     quit()
-print('[信息]正在验证程序包...')
+print('[App][Core][信息]正在验证程序包...')
 if verify == '[ICsetupfile]':
     pass
 else:
-    print('[警告]配置文件没有被授权，请检查setup.py来源是否可靠')
-    input('[等待]若要忽略请回车->')
-print('[信息]正在检查运行次数')
+    print('[App][Core][警告]配置文件没有被授权，请检查setup.py来源是否可靠')
+    input('[App][Core][等待]若要忽略请回车->')
+print('[App][Core][信息]正在检查运行次数')
 if first == '1':
     firstrun=1
     with open(path+'\config.ICset')as fp:
@@ -51,8 +55,8 @@ if first == '1':
         fob.close()
 else:
     firstrun=0
-print('[信息]运行前检查已完成，正在启动天气模块')
-print('[信息]天气模块已启动，正在获取...')
+print('[App][Core][信息]运行前检查已完成，正在启动天气模块')
+print('[App][Weather][信息]天气模块已启动，正在获取...')
 rep = requests.get('http://www.tianqiapi.com/api?version=v6&appid=99245552&appsecret=RL8NtmPx&city='+target)
 rep.encoding = 'utf-8'
 city=rep.json()['city']
@@ -61,15 +65,15 @@ temp=rep.json()['tem']+'°C'
 wspeed=rep.json()['win_speed']
 hum=rep.json()['humidity']
 airlevel=rep.json()['air_level']
-print('[信息]获取成功')
+print('[App][Weather][信息]获取成功')
 root=tkinter.Tk()
-print('[信息]主窗口已创建')
+print('[App][WMG][信息]主窗口已创建')
 w = root.winfo_screenwidth()
 h = root.winfo_screenheight()
 root.geometry("%dx%d" %(w,h))
 root.title('[已锁定]智能衣物管理系统')
 root.attributes("-fullscreen", 'true')
-print('[信息]正在加载启动窗口模块')
+print('[App][WMG][信息]正在加载启动窗口模块')
 w1=tkinter.Label(root,text=city,font='微软雅黑 -30')
 w2=tkinter.Label(root,text='天气:'+wea,font='微软雅黑 -20')
 w3=tkinter.Label(root,text='温度:'+temp,font='微软雅黑 -20')
@@ -78,7 +82,7 @@ w5=tkinter.Label(root,text='风速:'+wspeed,font='微软雅黑 -20')
 w6=tkinter.Label(root,text='空气质量:'+airlevel,font='微软雅黑 -20')
 b1=tkinter.Label(root,text=' ',font='微软雅黑 -120')
 b2=tkinter.Label(root,text=' ',font='微软雅黑 -20')
-print('[信息]正在获取时间信息')
+print('[App][Time][信息]正在获取时间信息')
 t1=tkinter.Label(compound='center',text=time.strftime('%H:%M',time.localtime(time.time())),font='华文彩云 -120')
 def trickit():
     currentTime=time.strftime('%H:%M',time.localtime(time.time()))
@@ -91,7 +95,7 @@ def unlock():
         os.execv(__file__, sys.argv)
     root.destroy()
     main=tkinter.Tk()
-    print('[信息]新窗口已创建')
+    print('[App][WMG][信息]新窗口已创建')
     main.title('智能衣物管理系统')
     main.geometry("%dx%d" %(w,h))
     main.attributes("-fullscreen", 'true')
@@ -112,7 +116,7 @@ def unlock():
     bt2=tkinter.Button(main,text='锁定系统',font='微软雅黑 -30',command=restart_program)
     bt2.grid(row=0,column=4)
 bt1=ttk.Button(root,text='                                  轻触来解锁                                  ',command=unlock)
-print('[信息]正在应用启动窗口模块')
+print('[App][WMG][信息]正在应用启动窗口模块')
 b1.pack()
 t1.pack()
 w1.pack()
@@ -123,4 +127,5 @@ w5.pack()
 w6.pack()
 b2.pack()
 bt1.pack()
+print('[App][Core][警告]加载完成')
 root.mainloop()
